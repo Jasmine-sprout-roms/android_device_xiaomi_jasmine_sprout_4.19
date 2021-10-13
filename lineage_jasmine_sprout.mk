8389c1bcb7d0ac23afbad5d028a5dc9f938b73cd
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-10-13 07:04:52 +0530

    add fork lineageOS specific command

--- lineage_jasmine_sprout.mk
+++ lineage_jasmine_sprout.mk
@@ -51,3 +51,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+
+TARGET_FLOS := true
